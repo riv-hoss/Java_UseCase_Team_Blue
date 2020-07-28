c9a7d72ad0de1f6926949a3708f25de930a87202
Farzaneh Younesi <farzanehynsi@gmail.com>
2020-07-28 15:23:55 +0200

    Done.

--- src/Employee.java
+++ src/Employee.java
@@ -18,9 +18,67 @@ public class Employee extends User {
         this.description = description;
     }
 
+    public String getFullName() {
+        return fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getiD() {
+        return iD;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 
 
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", iD=" + iD +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
 
 
