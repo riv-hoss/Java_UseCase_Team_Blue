7bba7862fd061e39e31f55c6b6ce47733dcd9e2c
fyzam <fyzam@fasy.at>
2020-07-28 15:20:13 +0200

    add Manager Class

---
+++ src/Manager.java
@@ -0,0 +1,38 @@
+public class Manager extends User{
+
+	public Manager(String firstName, String lastName, String eMail, String address, String zip, String city, String phone) {
+		super(firstName, lastName, eMail, address, zip, city, phone);
+	}
+
+
+	public void addEmployee(Employee employee){
+
+	}
+
+	public void removeEmployee(int employeeId){
+
+	}
+
+	public void addAnimal(Animal animal){
+
+	}
+
+	public void removeAnimal(int animalId){
+
+	}
+
+	public void displayMedicationPerAnimal(int AnimalId){
+		//SUM Cost
+	}
+
+	public void displayMedicationPerSpecies(int AnimalId){
+		//SUM Cost
+	}
+
+	public void displayWeeklyStatusReports(){
+		//mix of the obove
+	}
+
+	//ressourceManagement nice to have
+
+}
