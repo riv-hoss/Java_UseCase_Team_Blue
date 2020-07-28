d177e4d52ea154837a5ef3f0b258579a44d28365
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-07-28 15:50:33 +0200

    dd

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        Zoo x = new Zoo();
+
+        x.menu();
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Zoo {
 
@@ -10,6 +11,54 @@ public class Zoo {
       this.name = name;
       this.address = address;
 
+    }
+
+
+    Scanner scan = new Scanner(System.in);
+    Zoo(){
+    menu();
+    }
+    public void menu(){
+        System.out.println("Animal healthy request");
+        System.out.println("1) Animals at the vet");
+        System.out.println("2) Animals in special care.");
+        System.out.println("3) At the Vet ranking for species\n");
+        System.out.println("ZOO Status reyuests");
+        System.out.println("4) Animals per Species");
+        System.out.println("5)Number of Vet Interventions");
+        System.out.println("6) Number of sick Animals since Zoo opening");
+        System.out.println("7) Number of animals who needed special care since Zoo opening\n");
+        System.out.println("Actions");
+        System.out.println("8) Send to vet.");
+
+        int inputMenu = scan.nextInt();
+
+        switch (inputMenu) {
+            case 1:
+                System.out.println("1) Animals at the vet");
+                break;
+            case 2:
+                System.out.println("2) Animals in special care.");
+                break;
+            case 3:
+                System.out.println("3) At the Vet ranking for species\n");
+                break;
+            case 4:
+                System.out.println("4) Animals per Species");
+                break;
+            case 5:
+                System.out.println("5)Number of Vet Interventions");
+                break;
+            case 6:
+                System.out.println("6) Number of sick Animals since Zoo opening");
+                break;
+            case 7:
+                System.out.println("7) Number of animals who needed special care since Zoo opening\n");
+                break;
+            case 8:
+                System.out.println("8) Send to vet.");
+                break;
+        }
     }
 
     HashMap <Integer, String> users = new HashMap<>();
@@ -46,6 +95,7 @@ public class Zoo {
         animals.remove(id);
         System.out.println("Animal is removed from list");
     }
+
 
     public void printAnimal(){
         for (int i : animals) {
