cd5db59bdf575fc833e0b5dfb7b6c5360ece2591
fyzam <fyzam@fasy.at>
2020-07-28 14:40:02 +0200

    add user ethods skeletons

--- src/User.java
+++ src/User.java
@@ -85,4 +85,62 @@ public class User {
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
+	public void displayCurrentlySickAnimals() {
+		//plus SUM
+	}
+
+	public void displayCurrentlyInCareAnimals() {
+		//plus SUM
+	}
+
+	public void displaySumOfVetVisitsEver() {
+
+	}
+
+	public void displaySumOfSickAnimalsEver() {
+
+	}
+
+	public void displaySumOfSpecialCareAnimalsEver() {
+
+	}
+
+	public void displayVetHistoryPerAnimal() {
+
+	}
+
+	public void displayVetHistoryPerSpecies() {
+
+	}
+
+	public void displaySumOfVetVisitsPerSAnimal() {
+
+	}
+
+	public void displaySumOfVetVisitsPerSpecies() {
+
+	}
+
+	public void sendAnimalToVet() {
+
+	}
+
+	public void sendAnimalToSpecialCare() {
+
+	}
+
+	public void receiveAnimalBack() {
+
+	}
+
+	public void displaySumOfTotalAnimals() {
+
+	}
+
+	public void displaySumOfAnimalsPerSpecies() {
+
+	}
+
+
 }
