ac1ea2a2132c64faa66b7a51afb75afa96769f04
fyzam <fyzam@fasy.at>
2020-07-29 09:38:51 +0200

    park methods to implement in temporary class

---
+++ src/MethodsToImplement.java
@@ -0,0 +1,61 @@
+public class MethodsToImplement {
+
+	public void displayCurrentlySickAnimals() {
+		//plus SUM
+	}
+
+	public void displayCurrentlyInCareAnimals() {
+		//plus SUM
+	}
+
+	public void displaySumOfVetVisitsEver() {
+
+	}
+
+	public void displaySumOfSickAnimalsEver() {
+
+	}
+
+	public void displaySumOfSpecialCareAnimalsEver() {
+
+	}
+
+	public void displayVetHistoryPerAnimal() {
+
+	}
+
+	public void displayVetHistoryPerSpecies() {
+
+	}
+
+	public void displaySumOfVetVisitsPerSAnimal() {
+
+	}
+
+	public void displaySumOfVetVisitsPerSpecies() {
+
+	}
+
+	public void sendAnimalToVet() {
+
+	}
+
+	public void sendAnimalToSpecialCare() {
+
+	}
+
+	public void receiveAnimalBack() {
+
+	}
+
+	public void displaySumOfTotalAnimals() {
+
+	}
+
+	public void displaySumOfAnimalsPerSpecies() {
+
+	}
+
+
+
+}
