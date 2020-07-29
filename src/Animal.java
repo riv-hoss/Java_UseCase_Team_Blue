1d4ec8f985c2dcc7b386bf5292c2d3fbcf7e6933
Florian Mörzinger <flo.moerzinger@gmx.at>
2020-07-29 13:31:05 +0200

    dd

--- src/Animal.java
+++ src/Animal.java
@@ -1,26 +1,48 @@
 import java.util.ArrayList;
 
 
-
 public class Animal {
-    private String name;
     private static int counter = 0;
+    private int vetCounter = 0, specialCareCounter = 0;
     private int id;
+    private String name;
     private int vetCount = 0;
     private int specialCareCount = 0;
+    private boolean atTheVet;
+    private boolean atSpecialCare;
     private String species;
     private String healthStatus;
-    private ArrayList<VetHistory> vetHistory = new ArrayList<>();
+    private ArrayList<String> vetHistory = new ArrayList<>();
 
 
-
-    public Animal(String name, String species, String healthStatus) {
+    public Animal(String name, String species, String healthStatus, boolean atTheVet, boolean atSpecialCare) {
         this.name = name;
         this.species = species;
         this.healthStatus = healthStatus;
+        this.atTheVet = atTheVet;
+        this.atSpecialCare = atSpecialCare;
         this.id = ++counter;
     }
 
+
+    public int VetCounter(){
+        vetCount = vetCounter++;
+        //System.out.println(vetCount);
+       return vetCount;
+    }
+
+
+    public int specialCareCounter(){
+        specialCareCount = specialCareCounter++;
+        //System.out.println(specialCareCount);
+       return specialCareCount;
+    }
+
+
+
+
+
+   /*
 
     public double getMedicationCost() {
 
@@ -82,5 +104,7 @@ public class Animal {
                 '}';
     }
 
+
+    */
     
 }
